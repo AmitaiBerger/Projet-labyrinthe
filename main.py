5ce71e58a09deb01e2820dab2427b4af917e3e48
Charles de Bourgoing <charles.debourgoing@gmail.com>
2025-11-10 14:14:08 +0100

    squelette général

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #import Laby
 from Laby import *
 
+def partie():
+    print("lancement d'une partie'")
+
+
 if __name__=="__main__":
     Labyr = Labyrinthe(10,10)
     Labyr.generer_par_Wilson()
@@ -9,5 +13,10 @@ if __name__=="__main__":
     Labyr.visibles()
     print("vision  depuis la case 5,5 :", Labyr.cases[5*Labyr.largeur+5].visibles)
     print("vision  depuis la case 4,4 :", Labyr.cases[4*Labyr.largeur+4].visibles)
+
+    # menu principal, qui mène in fine à
+    partie()
+
+
 
     print("fin du programme")
